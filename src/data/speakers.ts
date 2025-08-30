8b45998354d64ccaba900fcb1eecba06dc9e96f7
Manuel <manuel.t.shade@gmail.com>
2025-08-31 00:34:45 +0100

    feat: :sparkles: add speakers data

---
+++ src/data/speakers.ts
@@ -0,0 +1,97 @@
+export const ponentes = [
+  {
+    name: 'Antonio Leiva',
+    role: '',
+    picture: '/speakers/antonio-leiva.jpg',
+    socials: {
+      twitter: 'https://x.com/devexpert_io',
+      linkedin: 'https://www.linkedin.com/in/antonio-devexpert',
+      instagram: 'https://www.instagram.com/devexpert.io/',
+      web: 'https://devexpert.io/',
+    },
+  },
+  {
+    name: 'Estefanía Peromingo',
+    role: '',
+    picture: '/speakers/estefania-peromingo.jpg',
+    socials: {
+      twitter: '',
+      linkedin:
+        'https://www.linkedin.com/in/estefaniaperomingo/',
+    },
+  },
+  {
+    name: 'Irene Ruiz Pozo',
+    role: '',
+    picture: '/speakers/irene-ruiz-pozo.jpg',
+    socials: {
+      twitter: '',
+      linkedin: 'https://linkedin.com/in/ireneruizpozo/',
+      instagram: 'https://www.instagram.com/ireneeruizp/',
+    },
+  },
+  {
+    name: 'María del Carmen Tajuelo',
+    role: '',
+    picture: '/speakers/maria-del-carmen-tajuelo.jpg',
+    socials: {
+      twitter: '',
+      linkedin: 'https://www.linkedin.com/in/carmentajuelo/',
+    },
+  },
+  {
+    name: 'Máximo Fernández Núñez',
+    role: '',
+    picture: '/speakers/maximo-fernandez-nunez.jpg',
+    socials: {
+      twitter: 'https://twitter.com/Maximo_fn',
+      linkedin: 'http://linkedin.com/in/MaximoFN/',
+      facebook:
+        'https://www.facebook.com/profile.php?id=100085177670661',
+      instagram: 'https://www.instagram.com/maximo__fn/',
+      web: 'https://maximofn.com/blog/',
+      huggingface: 'https://huggingface.co/Maximofn',
+    },
+  },
+  {
+    name: 'Moisés Martínez',
+    role: '',
+    picture: '/speakers/moises-martinez.jpg',
+    socials: {
+      twitter: 'https://x.com/moisipm?lang=es',
+      linkedin: 'https://linkedin.com/in/momartinm/',
+    },
+  },
+  {
+    name: 'Nacho Fernandez Diez',
+    role: '',
+    picture: '/speakers/nacho-fernandez-diez.png',
+    socials: {
+      twitter: '',
+      linkedin:
+        'https://www.linkedin.com/in/nacho-fernandez-diez-94763974',
+    },
+  },
+  {
+    name: 'Pablo Rodríguez-Tembleco',
+    role: '',
+    picture: '/speakers/pablo-rodriguez-tembleco.jpg',
+    socials: {
+      twitter: 'https://x.com/PauThePlayer',
+      linkedin:
+        'https://www.linkedin.com/in/pablo-rodriguez-tembleco/',
+      instagram: 'https://www.instagram.com/pautheplayer/',
+      youtube: 'https://www.youtube.com/@PauThePlayer',
+    },
+  },
+  {
+    name: 'Sergi Olives',
+    role: '',
+    picture: '/speakers/sergi-olives.jpg',
+    socials: {
+      twitter: 'https://x.com/SrAceituno',
+      linkedin: 'https://linkedin.com/in/sergiolives/',
+      instagram: 'https://www.instagram.com/mraceitun0/',
+    },
+  },
+];
